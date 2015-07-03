9f753142a8e748e801d1e09fb068d7081b9059b9
giang.nguyen <giang.nguyen@LGDN14039-W732.logigear.com>
2015-07-03 18:24:32 +0700

    tc09

---
+++ src/TestCases/ARTICLE/TO_JOOMLA_ARTICLE_009.java
@@ -0,0 +1,61 @@
+package TestCases.ARTICLE;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import Databases.Article;
+import Databases.User;
+import Functions.Abstract_test;
+import Functions.ReadData;
+import Pages.Article_page;
+import Pages.Factory_page;
+import Pages.Home_page;
+import Pages.Login_page;
+import Pages.NewArticle_page;
+
+public class TO_JOOMLA_ARTICLE_009 extends Abstract_test{
+
+	ReadData data = new ReadData();
+	User user = new User();
+	Article article = new Article();
+	
+	private Login_page loginPage;
+	private Home_page homePage;
+	private Article_page articlePage;
+	private NewArticle_page newArticlePage;
+	
+	@BeforeMethod
+	public void setup(){
+		String url = data.getUrl("Local_url");
+		user = data.getUser("Giang");
+		article = data.getArticle("Article1");
+		driver = openJoomla(url);
+	}
+	
+	@Test(description = "User can search for articles using the filter text field")
+	public void TC_009 (){
+		loginPage = Factory_page.getLoginPage(driver);
+		homePage = loginPage.loginValidAccount(user.getUsername(), user.getPassword(), "");
+		articlePage = homePage.navigatetoArticlepage();
+		newArticlePage = articlePage.openNewArticlepage();
+		
+		articlePage = newArticlePage.addNewArticle(article.getTitle(), article.getCategory(), "", article.getContent(),"");
+		log.info("Verify message Article successfully saved displayed");
+		verifyTrue(articlePage.isArticleDisplay(article.getTitle()));
+		
+		log.info("Search for an article");
+		articlePage.searchforArticle(article.getTitle());
+		
+		log.info("Verify the title of search article is partially matched with the entered keyword");
+		articlePage.isSearchArticleDisplay(article.getTitle());
+	}
+	
+	@AfterMethod
+	public void end(){
+		articlePage.deleteArticle(article.getTitle());
+		shutdown();
+	}
+}
+
