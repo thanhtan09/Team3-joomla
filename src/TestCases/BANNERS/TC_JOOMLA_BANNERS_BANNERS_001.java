1ab3a83fd2d90347bbd2d69009e8354a1559fa19
asus pc <asus pc@VictorVo>
2015-07-05 23:11:51 +0700

    Tan Vo

--- src/TestCases/BANNERS/TC_JOOMLA_BANNERS_BANNERS_001.java
+++ src/TestCases/BANNERS/TC_JOOMLA_BANNERS_BANNERS_001.java
@@ -4,32 +4,23 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import Databases.Article;
-import Databases.User;
 import Functions.Abstract_test;
-import Functions.ReadData;
-import Pages.Article_page;
+import Pages.Client_page;
 import Pages.Factory_page;
 import Pages.Home_page;
 import Pages.Login_page;
-import Pages.NewArticle_page;
+import Pages.NewClient_page;
 
 public class TC_JOOMLA_BANNERS_BANNERS_001 extends Abstract_test{
 
-	ReadData data = new ReadData();
-	User user = new User();
-	Article article = new Article();
 	private Login_page loginPage;
 	private Home_page homePage;
-	private Article_page articlePage;
-	private NewArticle_page newarticlePage;
+	private Client_page clientPage;
+	private NewClient_page newclientPage;
 	
 	@BeforeMethod
 	public void setup(){
-		String url = data.getUrl("Local_url");
-		user = data.getUser("Tan");
-		article = data.getArticle("Article1");
-		driver = openJoomla(url);
+		driver = openJoomla();
 	}
 	
 	@Test(description = "Verify user can create new article with valid information")
@@ -39,20 +30,15 @@ public class TC_JOOMLA_BANNERS_BANNERS_001 extends Abstract_test{
 		loginPage = Factory_page.getLoginPage(driver);
 		homePage = loginPage.loginValidAccount(user.getUsername(), user.getPassword(),"");
 		
-		log.info("Enter Article page");
-		articlePage = homePage.navigatetoArticlepage();		
+		clientPage = homePage.navigatetoCLientpage();		
+		newclientPage = clientPage.navigateNewCLientpage();
 		
-		log.info("Create new article");
-		newarticlePage = articlePage.openNewArticlepage();
-		articlePage = newarticlePage.addNewArticle(article.getTitle(), article.getCategory(), "",article.getContent(),"");
+		newclientPage.addClient(client.getName(), client.getContact(), client.getEmail(), "");
 		
-		log.info("Verify message Article successfully saved displayed");
-		verifyTrue(articlePage.isArticleDisplay(article.getTitle()));
 	}
 	
 	@AfterMethod
 	public void end(){
-		articlePage.deleteArticle(article.getTitle());
-		shutdown();
+		//shutdown();
 	}
 }
