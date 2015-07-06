66fdd296771af0c21c36959903f630c65cc062a7
giang.nguyen <giang.nguyen@LGDN14039-W732.logigear.com>
2015-07-06 20:34:01 +0700

    drtfsdf

---
+++ src/TestCases/ARTICLE/TC_JOOMLA_ARTICLE_012.java
@@ -0,0 +1,41 @@
+package TestCases.ARTICLE;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import Functions.Abstract_test;
+import Pages.Article_page;
+import Pages.Factory_page;
+import Pages.Home_page;
+import Pages.Login_page;
+import Pages.NewArticle_page;
+
+public class TC_JOOMLA_ARTICLE_012 extends Abstract_test{
+
+	private Login_page loginPage;
+	private Home_page homePage;
+	private Article_page articlePage;
+	private NewArticle_page newArticlePage;
+	
+	@BeforeMethod
+	public void setup(){
+		driver = openJoomla();
+	}
+	
+	@Test(description = "User can paging the article using the paging control")
+	public void TC_ARTICLE_012 (){
+		loginPage = Factory_page.getLoginPage(driver);
+		homePage = loginPage.loginValidAccount(user.getUsername(), user.getPassword(), "");
+		articlePage = homePage.navigatetoArticlepage();
+		articlePage.selectDisplayItem("5");
+		verifyTrue(articlePage.isPaging("5"));
+	}
+	
+	@AfterMethod
+	public void end(){
+		//articlePage.deleteArticle(article.getTitle());
+		shutdown();
+	}
+}
+
